cf98dcb5ad7703157a4094097f40b4a110f9653f
Rafael Navarro Gutierrez <ranavarr@c3r3s1.42malaga.com>
2025-02-27 18:47:59 +0100

    damn, son, it slowly gets better

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,24 +1,53 @@
-// empezemos de nuevo, tiremoslo todo a la basura
-# include "get_next_line.h"
+// empecemos de nuevo, tiremoslo todo a la basura
+#include "get_next_line.h"
 
-char *ft_strjoin(const char *s1, const char *s2)
+static void	*myrealloc(void *ptr, size_t size)
 {
-    char *res;
-    int i;
-    int j;
+	void	*out;
+	int 	i;
 
-    i = 0;
-    j = 0;
-    res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-    if (!res)
-        return (NULL);
-    while (s1[i])
-        res[j++] = s1[i++];
-    i = 0;
-    while (s2[i])
-        res[j++] = s2[i];
-    res[j] = 0;
-    return (res);
+	i = 0;
+	out = malloc(size);
+	if (!out)
+		return ;
+
+	while (ptr[i])
+	{
+		out[i] = ptr[i];
+		printf("%s\n",out);
+		i++;
+	}
+	return (out);
+}
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i];
+	res[j] = 0;
+	return (res);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -37,26 +66,14 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-int	ft_strlen(const char *s)
+int	main(void)
 {
-	int	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-
-int main(void)
-{
-	int			fd;		//	File descriptor we read from 
 	static char	*buf; 	//	Buffer where we store BUFFER_SIZE bytes read
+	/// static int	steps;	// 	Multiplyer to know the size of the new buffer // don't need this shi
+	int			fd;		//	File descriptor we read from 
 	int			bytes;	//	Return value of read()
 	char		*chr;	//	Return value of ft_strchr
 	char		*replacement;
-	
-
 
 	chr = 0;			// at least initialize it for hygiene
 	replacement = 0;	// at least initialize it for hygiene
@@ -74,8 +91,6 @@ int main(void)
 	}
 	// open file before reading it
 	fd = open("text", O_RDONLY);
-	
-	
 
 	// read BUFFER_SIZE bytes from the fd
 	bytes = read(fd, buf, BUFFER_SIZE);
@@ -93,7 +108,8 @@ int main(void)
 	if (chr == 0)	//	If ther is no newline in buf
 	{
 		printf("No newline found in buffer \n");
-		replacement = 
+		replacement = (char *)(myrealloc(buf, (strlen(buf + BUFFER_SIZE))));
+
 		// do the thing where we add another buffer and read again?
 	}
 	else
