a62cac581f43e257412a72bb0f006166829c76f7
Rafael Navarro Gutierrez <ranavarr@c2r1s5.42malaga.com>
2025-03-29 16:06:36 +0100

    Ill get it done next time

---
+++ theline/get_next_line.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 14:30:00 by ranavarr          #+#    #+#             */
+/*   Updated: 2025/03/29 16:05:56 by ranavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <get_next_line.h>
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (0);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t		i;
+
+	i = 0;
+	while ((i + 1 < size) && (src[i]))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i];
+	res[j] = 0;
+	return (res);
+}
+
+char	*substr(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != c)
+	{
+		i++;
+	}
+}
---
+++ theline/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 14:00:17 by ranavarr          #+#    #+#             */
+/*   Updated: 2025/03/29 16:04:06 by ranavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+
+# endif
+
+# include <limits.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char		*get_next_line(int fd);
+int			ft_strlen(const char *s);
+char		*ft_strchr(const char *s, int c);
+size_t		t_strlcpy(char *dst, const char *src, size_t size);
+char		*ft_strjoin(const char *s1, const char *s2);
+char		*substr(char *str, char c);
+
+#endif
