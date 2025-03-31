57acca47597bd5f75f08cd899fd9521fcbd59e3d
Rafael Navarro Gutierrez <ranavarr@c2r1s5.42malaga.com>
2025-03-31 13:39:19 +0200

    prints garbage but half works

--- theline/get_next_line.c
+++ theline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 14:30:00 by ranavarr          #+#    #+#             */
-/*   Updated: 2025/03/30 17:42:49 by ranavarr         ###   ########.fr       */
+/*   Updated: 2025/03/31 13:38:46 by ranavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	 return (ptr);
 }
 */
-
 int	ft_strlen(const char *s)
 {
 	int	len;
@@ -41,40 +40,23 @@ int	ft_strlen(const char *s)
 	return (len);
 }
 
-char	*ft_strchr(const char *s, int c)
+int	ft_strchr_pos(const char *s, char c)
 {
 	int	i;
 
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
+		if (s[i] == c)
+			return (i);
 		i++;
 	}
 	if (c == '\0')
-		return ((char *)&s[i]);
-	return (0);
+		return (i);
+	return (-1);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t		i;
-
-	i = 0;
-	while ((i + 1 < size) && (src[i]))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	if (size > 0)
-		dst[i] = '\0';
-	while (src[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin_free(char **s1, char *s2)
+char	*ft_strjoin_free(char **s1, char *s2, int len)
 {
 	char	*res;
 	int		i;
@@ -90,12 +72,88 @@ char	*ft_strjoin_free(char **s1, char *s2)
 	while ((*s1)[i])
 		res[j++] = (*s1)[i++];
 	i = 0;
-	while (s2[i])
+	while (s2[i] && i < len)
 		res[j++] = s2[i++];
 	res[j] = '\0';
 	free (*s1);
 	(*s1) = res;
 	return (res);
+}
+
+char	*line(const char *buffer, int pos)
+{
+	char	*theline;		
+	int		i;
+
+	i = 0;
+	theline = malloc(sizeof(char) * (pos + 1));
+	if (!theline)
+		return (NULL);
+	while (i < pos)
+	{
+		theline[i] = buffer[i];
+		i++;
+	}
+	theline[i] = '\0';
+}
+
+char	*shoveit(char *buffer, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i <= pos && i < BUFFER_SIZE)
+	{
+		buffer[i] = buffer[pos + i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char buffer[BUFFER_SIZE];
+	char		*line;
+	int			bytes;
+	int			nlpos;
+
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	if (bytes < 0)
+		return (NULL);
+	line = malloc(sizeof(char) * (1));
+	if (!line)
+		return (NULL);
+	line[0] = '\n';
+	nlpos = ft_strchr_pos(buffer, '\n');
+	while (nlpos < 0)
+	{
+		ft_strjoin_free(&line, buffer, BUFFER_SIZE);
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+			return (NULL);
+		nlpos = ft_strchr_pos(buffer, '\n');
+	}
+	ft_strjoin_free(&line, buffer, nlpos);
+	return (line);
+}
+
+/*
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t		i;
+
+	i = 0;
+	while ((i + 1 < size) && (src[i]))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
 }
 
 char	*substr(char *s, int len)
@@ -147,7 +205,7 @@ char	*shortbuffer(char **s, int pos)
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	buffer[BUFFER_SIZE];
 	char		*output;
 	char		*tmp;
 	char		*nlpos;
@@ -194,26 +252,21 @@ char	*get_next_line(int fd)
 	tmp = NULL;
 	return (output);
 }
+*/
 
 int	main(void)
 {
 	int		fd;
-	int		i;
-	//char	*buffer = "0123456789";
+	char	file[] = "get_next_line.c";
 
-
-	i = 0;
-	//substr
-	//printf("%s \n", substr("0123456", 3));
-	//shortbuffer
-//	buffer = shortbuffer(&buffer, 4);
-//	printf("%s \n", buffer);
-	fd = open("./get_next_line.c", O_RDONLY);
-	while (i < 3)
-	{
-		printf("line = %s \n", get_next_line(fd));
-		i++;
-	}		
+	fd = open(file, O_RDONLY);
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
+	printf("%s \n", get_next_line(fd));
 	close(fd);
 	return (0);
 }
--- theline/get_next_line.h
+++ theline/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 14:00:17 by ranavarr          #+#    #+#             */
-/*   Updated: 2025/03/30 17:06:33 by ranavarr         ###   ########.fr       */
+/*   Updated: 2025/03/30 20:43:53 by ranavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+#  define BUFFER_SIZE 1 
 # endif
 
 # include <limits.h>
